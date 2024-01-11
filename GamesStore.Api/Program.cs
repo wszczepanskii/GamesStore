14da89a8b09cfb14b42b1ba6c9d591c250606b19
bvmbo <wiktorsz2001@wp.pl>
2024-01-11 21:40:00 +0100

    changed to singleton service

--- GamesStore.Api/Program.cs
+++ GamesStore.Api/Program.cs
@@ -1,6 +1,9 @@
 using GamesStore.Api.Endpoints;
+using GamesStore.Api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<IGamesRepo, InMemGamesRepo>();
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
