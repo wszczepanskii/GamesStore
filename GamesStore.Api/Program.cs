4d97d686626019f0864de12ee59c4fa9491bc354
bvmbo <wiktorsz2001@wp.pl>
2024-01-11 18:38:33 +0100

    changed to groups

--- GamesStore.Api/Program.cs
+++ GamesStore.Api/Program.cs
@@ -32,8 +32,11 @@ List<Game> games = new (){
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/games", () => games);
-app.MapGet("/games/{id}", (int id) => {
+var group = app.MapGroup("/games").WithParameterValidation();
+
+group.MapGet("/", () => games);
+
+group.MapGet("/{id}", (int id) => {
     Game? game = games.Find(game => game.Id == id);
     
     if(game is null){
@@ -43,14 +46,14 @@ app.MapGet("/games/{id}", (int id) => {
 
 }).WithName(GetGameEndpointName);
 
-app.MapPost("/games", (Game game) => {
+group.MapPost("/", (Game game) => {
     game.Id = games.Max(game => game.Id) + 1;
     games.Add(game);
 
     return Results.CreatedAtRoute(GetGameEndpointName, new {id = game.Id}, game);
 });
 
-app.MapPut("/games/{id}", (int id, Game updatedGame) => {
+group.MapPut("/{id}", (int id, Game updatedGame) => {
     Game? existingGame = games.Find(game => game.Id == id);
     
     if(existingGame is null){
@@ -66,7 +69,7 @@ app.MapPut("/games/{id}", (int id, Game updatedGame) => {
     return Results.NoContent();
 });
 
-app.MapDelete("/games/{id}", (int id) => {
+group.MapDelete("/{id}", (int id) => {
     Game? game = games.Find(game => game.Id == id);
     
     if(game is not null){
